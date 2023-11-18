dead80fc1792bac5ad8dc9ba4d6f9a91b2c2e79f
Keana Cobarde <keanacobarde@gmail.com>
2023-11-18 09:23:21 -0600

    modifying settings menu slightly

--- components/NavBar.js
+++ components/NavBar.js
@@ -12,7 +12,7 @@ import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 
 const pages = ['Dashboard', 'Timeline'];
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+const settings = ['Logout'];
 
 function ResponsiveAppBar() {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
