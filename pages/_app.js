2152e99ef097aa077e935ce544cded6403219b88
Keana Cobarde <keanacobarde@gmail.com>
2023-11-18 09:20:33 -0600

    setting theme

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,34 @@
 /* eslint-disable react/prop-types */
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/globals.css';
+import { createTheme, ThemeProvider } from '@mui/material';
 import { AuthProvider } from '../utils/context/authContext';
 import ViewDirectorBasedOnUserAuthStatus from '../utils/ViewDirector';
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#1A202B',
+    },
+  },
+  typography: {
+    fontFamily: 'system-ui',
+  },
+});
+
 function MyApp({ Component, pageProps }) {
   return (
-    <AuthProvider> {/* gives children components access to user and auth methods */}
-      <ViewDirectorBasedOnUserAuthStatus
+    <ThemeProvider theme={theme}>
+      <AuthProvider> {/* gives children components access to user and auth methods */}
+        <ViewDirectorBasedOnUserAuthStatus
         // if status is pending === loading
         // if status is logged in === view app
         // if status is logged out === sign in page
-        component={Component}
-        pageProps={pageProps}
-      />
-    </AuthProvider>
+          component={Component}
+          pageProps={pageProps}
+        />
+      </AuthProvider>
+    </ThemeProvider>
   );
 }
 
