741bc73917933d09a9936bcd18447700c69e0301
Keana Cobarde <keanacobarde@gmail.com>
2023-11-23 13:47:44 -0600

    adding test modal

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,7 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import Link from '@mui/material/Link';
+import { Modal } from '@mui/material';
 import { useAuth } from '../utils/context/authContext';
 import { getCategories } from '../api/categoryData';
 import { getIncome } from '../api/incomeData';
@@ -27,6 +28,18 @@ function Copyright() {
   );
 }
 
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
 const date = new Date();
 const month = date.toLocaleDateString('default', { month: 'long' });
 
@@ -36,6 +49,10 @@ export default function Dashboard() {
   const [categories, setCategories] = React.useState([]);
   const [income, setIncome] = React.useState([]);
   const [expenses, setExpenses] = React.useState([]);
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   const getAllTheCategories = () => {
     getCategories(user.uid).then(setCategories);
@@ -119,7 +136,22 @@ export default function Dashboard() {
               spacing={2}
               justifyContent="center"
             >
-              <Button variant="contained" color="primary" size="large">ADD AN EXPENSE</Button>
+              <Button variant="contained" color="primary" size="large" onClick={handleOpen}>ADD AN EXPENSE</Button>
+              <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+              >
+                <Box sx={style}>
+                  <Typography id="modal-modal-title" variant="h6" component="h2">
+                    Text in a modal
+                  </Typography>
+                  <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                    Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+                  </Typography>
+                </Box>
+              </Modal>
             </Stack>
           </Container>
         </Box>
