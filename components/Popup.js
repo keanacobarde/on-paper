6a2fcd2e6812b768839826e59f09a26a0352a4b3
Keana Cobarde <keanacobarde@gmail.com>
2023-11-29 21:05:57 -0600

    getting pop up to work

--- components/Popup.js
+++ components/Popup.js
@@ -1,10 +1,13 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
 import Button from '@mui/material/Button';
-import { Dialog, DialogContent } from '@mui/material';
+import { Dialog, DialogContent, IconButton } from '@mui/material';
 import DialogTitle from '@mui/material/DialogTitle';
+import EditNoteIcon from '@mui/icons-material/EditNote';
 
-export default function Popup({ buttonName, formTitle, formContent }) {
+export default function Popup({
+  buttonType, buttonName, formTitle, formContent,
+}) {
   const [open, setOpen] = React.useState(false);
 
   const handleClickOpen = () => {
@@ -17,9 +20,15 @@ export default function Popup({ buttonName, formTitle, formContent }) {
 
   return (
     <>
-      <Button variant="contained" onClick={handleClickOpen}>
-        {buttonName}
-      </Button>
+      { buttonType === 'edit' ? (
+        <IconButton aria-label="edit" onClick={handleClickOpen}>
+          <EditNoteIcon />
+        </IconButton>
+      ) : (
+        <Button variant="contained" onClick={handleClickOpen}>
+          {buttonName}
+        </Button>
+      )}
       <Dialog open={open} onClose={handleClose}>
         <DialogTitle>{formTitle}</DialogTitle>
         <DialogContent>
@@ -31,7 +40,13 @@ export default function Popup({ buttonName, formTitle, formContent }) {
 }
 
 Popup.propTypes = {
-  buttonName: PropTypes.string.isRequired,
+  buttonType: PropTypes.string,
+  buttonName: PropTypes.string,
   formTitle: PropTypes.string.isRequired,
   formContent: PropTypes.element.isRequired,
+};
+
+Popup.defaultProps = {
+  buttonType: '',
+  buttonName: '',
 };
