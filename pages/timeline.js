f1682e3d1c1b6934ac7734e40e34aab4be273353
Keana Cobarde <keanacobarde@gmail.com>
2023-11-30 22:33:44 -0600

    adding month switching

--- pages/timeline.js
+++ pages/timeline.js
@@ -1,7 +1,15 @@
+import { Stack, Button } from '@mui/material';
 import React from 'react';
 
-export default function Timeline() {
+export default function timeline() {
+  const monthsArray = [
+    'Jan.', 'Feb.', 'Mar.', 'Apr.', 'May', 'June',
+    'July', 'Aug.', 'Sept.', 'Oct.', 'Nov.', 'Dec.',
+  ];
+
   return (
-    <div>timeline</div>
+    <Stack sx={{ mt: 4 }} spacing={2} direction="row">
+      {monthsArray.map((month) => <Button variant="outlined">{month}</Button>)}
+    </Stack>
   );
 }
