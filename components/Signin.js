d3d69d759d87f24f056acfea60419b1f85c0d01a
Keana Cobarde <keanacobarde@gmail.com>
2023-12-02 11:25:21 -0600

    changing signin welcome message

--- components/Signin.js
+++ components/Signin.js
@@ -13,8 +13,7 @@ function Signin() {
         margin: '0 auto',
       }}
     >
-      <h1>Hi there!</h1>
-      <p>Click the button below to login!</p>
+      <h1>Welcome to OnPaper!</h1>
       <Button type="button" size="lg" className="copy-btn" onClick={signIn}>
         Sign In
       </Button>
