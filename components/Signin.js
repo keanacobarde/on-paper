affb5baa37b0c16f2d60168cb1d1a51e57e8e7c9
Keana Cobarde <keanacobarde@gmail.com>
2023-12-08 15:32:15 -0600

    finalizing signin page draft

--- components/Signin.js
+++ components/Signin.js
@@ -14,7 +14,7 @@ function Copyright(props) {
   return (
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
       {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
+      <Link color="inherit" href="https://github.com/keanacobarde">
         OnPaper
       </Link>{' '}
       {new Date().getFullYear()}
