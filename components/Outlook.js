3e9ee517bc9bdd011192c898bbf6441adbabee1d
Keana Cobarde <keanacobarde@gmail.com>
2023-12-09 13:05:32 -0600

    building skeleton of Outlook page

---
+++ components/Outlook.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Grid, Typography } from '@mui/material';
+
+export default function Outlook() {
+  return (
+    <>
+      <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+        <Typography
+          component="h1"
+          variant="h1"
+          align="left"
+          color="text.primary"
+          gutterBottom
+        >
+          Outlook
+        </Typography>
+      </Grid>
+      <Grid container spacing={4} />
+    </>
+  );
+}
