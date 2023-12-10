030fc58d3b1a9f4d3046cb5c72e9d1bf7d07cbd2
Keana Cobarde <keanacobarde@gmail.com>
2023-12-10 17:46:56 -0600

    getting baseline doughnuit chart setup

--- components/Outlook.js
+++ components/Outlook.js
@@ -1,9 +1,38 @@
-import React from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useState, useEffect } from 'react';
 import {
   Card, Grid, Typography, CardContent,
 } from '@mui/material';
+import { Doughnut } from 'react-chartjs-2';
+import { getCategories } from '../api/categoryData';
+import { useAuth } from '../utils/context/authContext';
 
 export default function Outlook() {
+  // Obtain Data for Doughnut Chart - establish necessary hooks
+  const [categories, setCategories] = useState([]);
+  const { user } = useAuth();
+
+  const getAllTheCateogries = () => {
+    getCategories(user.uid).then(setCategories);
+  };
+
+  useEffect(() => {
+    getAllTheCateogries();
+  }, []);
+
+  const categoryData = {
+    labels: categories.map((category) => category.name),
+    datasets: [
+      {
+        label: 'Spending Limit',
+        data: categories.map((category) => category.spendingLimit),
+        backgroungColor: [
+
+        ],
+      },
+    ],
+  };
+
   return (
     <>
       <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
@@ -23,6 +52,7 @@ export default function Outlook() {
             <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
               Monthly Earnings
             </Typography>
+            <Doughnut data={categoryData} />
           </CardContent>
         </Card>
         <Card item sx={{ minWidth: 275 }}>
