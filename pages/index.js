2fbae2467c597841dba1e1b08617e80ab3304251
Keana Cobarde <keanacobarde@gmail.com>
2023-12-16 10:45:09 -0600

    modifying the conditional for the time difference

--- pages/index.js
+++ pages/index.js
@@ -33,7 +33,7 @@ export default function Dashboard() {
   // Finding necessary user data for rendering purposes
   const checkingAccAge = () => {
     const timeDiff = parseFloat(user.metadata.b) - parseFloat(user.metadata.a);
-    if (timeDiff < 5) {
+    if (timeDiff < 2) {
       const monthsArray = [
         'January', 'February', 'March', 'April', 'May', 'June',
         'July', 'August', 'September', 'October', 'November', 'December',
